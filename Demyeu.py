754fb9fe7b2ee6cd2707502566ca8b6c8e3eacfd
Thanh14p2 <105790840+Thanh14p2@users.noreply.github.com>
2023-04-19 11:34:06 +0700

    Add files via upload

---
+++ Demyeu.py
@@ -0,0 +1,12 @@
+from time import sleep
+from colorama import Fore, Back, Style
+x = input("nhập tên người bạn yêu:")
+z=input("nhập tên bạn:")
+y = 0
+while(1000):
+  print(Fore.YELLOW +    "❤️" , z ,"Yêu" ,x ,y ,"lần","❤️"   )
+  y = y+1
+  sleep(0.005)
+  
+print("nhưng em không yêu Tiến")
+  
